719185ce02c029eb335b3690f217523524aecc50
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-02 15:54:35 +0100

    bonus(dadi) terminato con utilizzo di if

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -1,7 +1,16 @@
-//genero un numero randomico da 1 a 6 per il giocatore
+//genero un numero randomico da 1 a 6 per il giocatore e lo stampo
 var dadi_giocatore = Math.floor(Math.random() * 6) + 1 ;
-console.log(dadi_giocatore);
+document.getElementById('lancio-giocatore').innerHTML = "Il tuo lancio è di " + dadi_giocatore;
 
 //genero un numero randomico da 1 a 6 per il computer
 var dadi_computer = Math.floor(Math.random() * 6) + 1 ;
-console.log(dadi_computer);
+document.getElementById('lancio-computer').innerHTML = "Il lancio del computer è di " + dadi_computer;
+
+//stampo esito nel DOM nell'elemento con id risultato
+if (dadi_giocatore > dadi_computer) {
+  document.getElementById('risultato').innerHTML = "Hai vinto!";
+} else if (dadi_giocatore < dadi_computer) {
+  document.getElementById('risultato').innerHTML = "Hai perso!";
+} else {
+  document.getElementById('risultato').innerHTML = "Pareggio!";
+}
